e75e9fc7f470cc533c5b776b86531d5d96590e98
Hiren Viradiya <hirenvjviradiya@gmail.com>
2022-03-30 15:23:23 +0530

    update code in apihub for dashboard

--- www/eblapihub/app/Http/Controllers/Api/DashboardController.php
+++ www/eblapihub/app/Http/Controllers/Api/DashboardController.php
@@ -12,15 +12,36 @@ use Illuminate\Http\Request;
 class DashboardController extends ApiController
 {
     /**
+    * The var implementation.
+    *
+    */
+    protected $userModel, $tempModel, $permanentModel;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct( User $userModel, TempDeviceModel $tempModel, PermanentModel $permanentModel)
+    {
+        $this->userModel            = $userModel;
+        $this->tempModel            = $tempModel;
+        $this->permanentModel       = $permanentModel;
+    }
+
+    /**
      * Get user and devices count
      *
      * @return \Illuminate\Http\JsonResponse
      */
     public function info(){
         $data = [];
-        $data['userCount'] = User::count();
-        $data['tempCount'] = TempDeviceModel::count();
-        $data['PermanentCount'] = PermanentModel::count();
+        $data['userCount'] = $this->userModel::count();
+        $data['tempCount'] = $this->tempModel::count();
+        $data['PermanentCount'] = $this->permanentModel::count();
+        // $data['userCount'] = User::count();
+        // $data['tempCount'] = TempDeviceModel::count();
+        // $data['PermanentCount'] = PermanentModel::count();
 
         if(($data)){
             $response['message'] = trans('api.messages.login.success');
